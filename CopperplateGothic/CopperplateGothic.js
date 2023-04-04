56447c83abe327a0dd36675a6ecfdcb744b4017e
Meridianer <115781775+Meridianer@users.noreply.github.com>
2023-04-04 14:46:21 +0800

    Update CopperplateGothic.js
    123

--- CopperplateGothic/CopperplateGothic.js
+++ CopperplateGothic/CopperplateGothic.js
@@ -149,7 +149,7 @@ console.log(document.getElementsByClassName('app-wrapper-web')[0].getElementsByC
 
 				document.querySelector(".landing-title").style.fontWeight = "bold";
 				document.querySelector(".landing-title").style.fontSize = "17px";
-				document.querySelector("._2I5ox").style.marginTop = '10px';
+				document.querySelector("._2I5ox").style.marginTop = '5px';
 				document.querySelector("._2I5ox").style.marginBottom = '0px';
 
 				 var domm = document.getElementsByClassName('_3JRy8')[1];
